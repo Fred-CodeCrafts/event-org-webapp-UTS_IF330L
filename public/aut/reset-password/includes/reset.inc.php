c67cb267f2edac1ba88a6aef65cb33307a434c19
Valenangelina <144827098+Valenangelina@users.noreply.github.com>
2024-10-25 07:43:15 +0700

    reset.inc.php

--- public/aut/reset-password/includes/reset.inc.php
+++ public/aut/reset-password/includes/reset.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 require '../../assets/includes/security_functions.php';
@@ -10,141 +9,81 @@ require '../../assets/setup/env.php';
 require '../../assets/setup/db.inc.php';
 
 if (isset($_POST['resetsubmit'])) {
-    foreach($_POST as $key => $value){
-
+    foreach ($_POST as $key => $value) {
         $_POST[$key] = _cleaninjections(trim($value));
     }
-
-    if (!verify_csrf_token()){
-
-        $_SESSION['STATUS']['resetsubmit'] = 'Request could not be validated';
-        header("Location: " . $_SERVER['HTTP_REFERER']);
-        exit();
-    }
-
 
     $selector = $_POST['selector'];
     $validator = $_POST['validator'];
     $password = $_POST['newpassword'];
-    $passwordRepeat = $_POST['confirmpassword'];    
-
+    $passwordRepeat = $_POST['confirmpassword'];
+    if (strlen($selector) !== 16 || strlen($validator) !== 64) {
+        $_SESSION['STATUS']['resentsend'] = 'Invalid token format, Please use a new reset email';
+        header("Location: ../");
+        exit();
+    }
+    error_log("Selector: " . $selector);
+    error_log("Validator: " . $validator);
     if (empty($selector) || empty($validator)) {
-
         $_SESSION['STATUS']['resentsend'] = 'Invalid token, Please use new reset email';
         header("Location: ../");
         exit();
     }
     if (empty($password) || empty($passwordRepeat)) {
-
         $_SESSION['ERRORS']['passworderror'] = 'Passwords cannot be empty';
         header("Location: " . $_SERVER['HTTP_REFERER']);
         exit();
-    }
-    else if ($password != $passwordRepeat) {
-
-        $_SESSION['ERRORS']['passworderror'] = 'Passwords donot match';
+    } else if ($password != $passwordRepeat) {
+        $_SESSION['ERRORS']['passworderror'] = 'Passwords do not match';
         header("Location: " . $_SERVER['HTTP_REFERER']);
         exit();
     }
-
-    $sql = "SELECT * FROM auth_tokens WHERE auth_type='password_reset' AND selector=? AND expires_at >= NOW() LIMIT 1";
+    $sql = "SELECT * FROM users WHERE password_reset_token = ? OR password_reset_expires_at > NOW();";
     $stmt = mysqli_stmt_init($conn);
 
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-
         $_SESSION['ERRORS']['scripterror'] = 'SQL ERROR';
         header("Location: " . $_SERVER['HTTP_REFERER']);
         exit();
-    }
-    else {
-
+    } else {
         mysqli_stmt_bind_param($stmt, "s", $selector);
         mysqli_stmt_execute($stmt);
         $results = mysqli_stmt_get_result($stmt);
 
         if (!($row = mysqli_fetch_assoc($results))) {
-
             $_SESSION['STATUS']['resentsend'] = 'Non-existent or expired token, Please use new reset email';
             header("Location: ../");
             exit();
-        }
-        else {
-
+        } else {
             $tokenBin = hex2bin($validator);
-            $tokenCheck = password_verify($tokenBin, $row['token']);
+            $tokenCheck = password_verify($tokenBin, $row['password_reset_token']);
 
             if ($tokenCheck === false) {
-
                 $_SESSION['STATUS']['resentsend'] = 'Invalid token, Please use new reset email';
                 header("Location: ../");
                 exit();
-            }
-            else if ($tokenCheck === true) {
+            } else if ($tokenCheck === true) {
+                $newPwdHash = password_hash($password, PASSWORD_DEFAULT);
+                $tokenEmail = $row['email'];
 
-                $tokenEmail = $row['user_email'];
-
-                $sql = 'SELECT * FROM users WHERE email=?;';
+                $sql = 'UPDATE users SET password=?, password_reset_token=NULL, password_reset_expires_at=NULL WHERE email=?;';
                 $stmt = mysqli_stmt_init($conn);
-
-                if (!mysqli_stmt_prepare($stmt, $sql)){
-
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
                     $_SESSION['ERRORS']['scripterror'] = 'SQL ERROR';
                     header("Location: " . $_SERVER['HTTP_REFERER']);
                     exit();
-                }
-                else {
-
-                    mysqli_stmt_bind_param($stmt, "s", $tokenEmail);
+                } else {
+                    mysqli_stmt_bind_param($stmt, "ss", $newPwdHash, $tokenEmail);
                     mysqli_stmt_execute($stmt);
-                    $results = mysqli_stmt_get_result($stmt);
 
-                    if (!$row = mysqli_fetch_assoc($results)) {
-                        
-                        $_SESSION['STATUS']['resentsend'] = 'Invalid token, Please use new reset email';
-                        header("Location: ../");
-                        exit();
-                    }
-                    else {
-                        
-                        $sql = 'UPDATE users SET password=? WHERE email=?;';
-                        $stmt = mysqli_stmt_init($conn);
-                        if (!mysqli_stmt_prepare($stmt, $sql))
-                        {
-                            $_SESSION['ERRORS']['scripterror'] = 'SQL ERROR';
-                            header("Location: " . $_SERVER['HTTP_REFERER']);
-                            exit();
-                        }
-                        else {
-
-                            $newPwdHash = password_hash($password, PASSWORD_DEFAULT);
-                            mysqli_stmt_bind_param($stmt, "ss", $newPwdHash, $tokenEmail);
-                            mysqli_stmt_execute($stmt);
-                            
-                            $sql = "DELETE FROM auth_tokens WHERE user_email=? AND auth_type='password_reset';";
-                            $stmt = mysqli_stmt_init($conn);
-                            if (!mysqli_stmt_prepare($stmt, $sql)){
-
-                                $_SESSION['ERRORS']['scripterror'] = 'SQL ERROR';
-                                header("Location: " . $_SERVER['HTTP_REFERER']);
-                                exit();
-                            }
-                            else{
-
-                                mysqli_stmt_bind_param($stmt, "s", $tokenEmail);
-                                mysqli_stmt_execute($stmt);
-                                
-                                $_SESSION['STATUS']['loginstatus'] = 'Password updated, Please log in';
-                                header ("Location: ../../login/");
-                            }
-                        }
-                    }
+                    $_SESSION['STATUS']['loginstatus'] = 'Password updated, Please log in';
+                    header("Location: ../../login/");
+                    exit();
                 }
             }
         }
     }
-}
-else {
-
+} else {
     header("Location: ../");
-    exit();
+    exit();
 }
