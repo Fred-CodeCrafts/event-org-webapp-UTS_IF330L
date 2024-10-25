c2ab473226ea3225e1af51b791d9494eccaccb6c
Valenangelina <144827098+Valenangelina@users.noreply.github.com>
2024-10-25 09:06:49 +0700

    db.inc.php

--- public/aut/assets/setup/db.inc.php
+++ public/aut/assets/setup/db.inc.php
@@ -1,28 +1,20 @@
 <?php
 
 require 'env.php';
-
-// kalau mau pake ini tinggal connectDB()->....
 function connectDB() {
-    try {
-        define('DB_NAME', 'utslec');
-        define('DB_USER', 'root');
-        define('DB_PASS', '');
-        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
-        $pdo = new PDO($dsn, DB_USER, DB_PASS);
-        
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        
-        return $pdo;
-    } catch(PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
+    define('DB_NAME', 'hary8495_utslec');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'hary8495_alenkeren');
+    define('DB_PASS', 'alenkeren');
+    
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
+
+    return $conn;
 }
 
-$conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$conn = connectDB();
 
-if (!$conn)
-{
-    die("Connection failed: ". mysqli_connect_error());
-}
