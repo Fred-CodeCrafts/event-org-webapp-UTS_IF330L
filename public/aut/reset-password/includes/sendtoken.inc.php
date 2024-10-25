ac2f5bdc02892ed3e21bd1562fbc900912c846b2
Valenangelina <144827098+Valenangelina@users.noreply.github.com>
2024-10-25 07:42:16 +0700

    sendtoken.inc.php

--- public/aut/reset-password/includes/sendtoken.inc.php
+++ public/aut/reset-password/includes/sendtoken.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 require '../../assets/includes/security_functions.php';
@@ -16,107 +15,78 @@ require '../../assets/vendor/PHPMailer/src/Exception.php';
 require '../../assets/vendor/PHPMailer/src/PHPMailer.php';
 require '../../assets/vendor/PHPMailer/src/SMTP.php';
 
-
 if (isset($_POST['resentsend'])) {
 
-    foreach($_POST as $key => $value){
-
+    // Clean input
+    foreach($_POST as $key => $value) {
         $_POST[$key] = _cleaninjections(trim($value));
     }
 
-    if (!verify_csrf_token()){
-
-        $_SESSION['STATUS']['resentsend'] = 'Request could not be validated';
-        header("Location: ../");
-        exit();
-    }
-
-
+    // Generate selector, token, and URL for reset
     $selector = bin2hex(random_bytes(8));
     $token = random_bytes(32);
-    $url = "localhost/loginsystem/reset-password/?selector=" . $selector . "&validator=" . bin2hex($token);
-    $expires = 'DATE_ADD(NOW(), INTERVAL 1 HOUR)';
+    $url = "https://listwannabewebapp.my.id/harsacompany/reset-password/?selector=".$selector."&validator=".bin2hex($token);
+    $expires = date("Y-m-d H:i:s", strtotime('+1 hour'));
 
     $email = $_POST['email'];
 
-    $sql = "SELECT id FROM users WHERE email=?;";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)){
-
-        $_SESSION['ERRORS']['sqlerror'] = 'SQL ERROR';
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['ERRORS']['emailerror'] = 'Invalid email address';
         header("Location: ../");
         exit();
     }
-    else {
 
+    $sql = "SELECT id FROM users WHERE email=?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        $_SESSION['ERRORS']['sqlerror'] = 'SQL ERROR';
+        header("Location: ../");
+        exit();
+    } else {
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_store_result($stmt);
 
-        if (mysqli_stmt_num_rows($stmt) == 0){
-
+        if (mysqli_stmt_num_rows($stmt) == 0) {
             $_SESSION['ERRORS']['emailerror'] = 'Given email does not exist in our records';
             header("Location: ../");
             exit();
         }
     }
 
-
-    $sql = "DELETE FROM auth_tokens WHERE user_email=? AND auth_type='password_reset';";
+    // Insert reset token into the database
+    $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+    $sql = "UPDATE users SET password_reset_token=?, password_reset_expires_at=? WHERE email=?;";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-
         $_SESSION['ERRORS']['sqlerror'] = 'SQL ERROR';
         header("Location: ../");
         exit();
-    }
-    else {
-
-        mysqli_stmt_bind_param($stmt, "s", $email);
-        mysqli_stmt_execute($stmt);
-    }
-
-
-    $sql = "INSERT INTO auth_tokens (user_email, auth_type, selector, token, expires_at) 
-            VALUES (?, 'password_reset', ?, ?, " . $expires . ");";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-
-        $_SESSION['ERRORS']['sqlerror'] = 'SQL ERROR';
-        header("Location: ../");
-        exit();
-    }
-    else {
-        
-        $hashedToken = password_hash($token, PASSWORD_DEFAULT);
-        mysqli_stmt_bind_param($stmt, "sss", $email, $selector, $hashedToken);
+    } else {
+        mysqli_stmt_bind_param($stmt, "sss", $hashedToken, $expires, $email);
         mysqli_stmt_execute($stmt);
     }
 
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
 
-
     $to = $email;
     $subject = 'Reset Your Password';
 
-    $mail_variables = array();
-
-    $mail_variables['APP_NAME'] = APP_NAME;
-    $mail_variables['email'] = $email;
-    $mail_variables['url'] = $url;
+    $mail_variables = array(
+        'APP_NAME' => APP_NAME,
+        'email' => $email,
+        'url' => $url
+    );
 
     $message = file_get_contents("./template_passwordresetemail.php");
-
-    foreach($mail_variables as $key => $value) {
-        
+    foreach ($mail_variables as $key => $value) {
         $message = str_replace('{{ '.$key.' }}', $value, $message);
     }
 
     $mail = new PHPMailer(true);
 
     try {
-
         $mail->isSMTP();
         $mail->Host = MAIL_HOST;
         $mail->SMTPAuth = true;
@@ -130,13 +100,11 @@ if (isset($_POST['resentsend'])) {
 
         $mail->isHTML(true);
         $mail->Subject = $subject;
-        $mail->Body    = $message;
+        $mail->Body = $message;
 
         $mail->send();
-    } 
-    catch (Exception $e) {
-        $_SESSION['STATUS']['mailstatus'] = 'Message could not be sent, try again later';
-
+    } catch (Exception $e) {
+        $_SESSION['STATUS']['mailstatus'] = 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
         header("Location: ../");
         exit();
     }
@@ -144,9 +112,7 @@ if (isset($_POST['resentsend'])) {
     $_SESSION['STATUS']['resentsend'] = 'Verification email sent';
     header("Location: ../");
     exit();
-}
-else {
-
+} else {
     header("Location: ../");
-    exit();
+    exit();
 }
