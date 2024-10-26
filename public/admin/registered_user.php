336852f398362c87d8faf3af103b21f36481ae5e
Je <jenniferjeo06@gmail.com>
2024-10-26 11:16:55 +0700

    fix bug

--- public/admin/registered_user.php
+++ public/admin/registered_user.php
@@ -147,18 +147,7 @@ function getUserEvents($userid){
         <?php endif; ?>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@9.0.3"></script>
 <script>
-    if (document.getElementById("tablee") && typeof simpleDatatables.DataTable !== 'undefined') {
-            const dataTable = new simpleDatatables.DataTable("#tablee", {
-                searchable: true,
-                sortable: true
-            });
-        }
-        document.addEventListener('DOMContentLoaded', () => {
-            const debouncedSearch = debounce(setupTableSearch, 300);
-            debouncedSearch();
-        });
 </script>
 </body>
 
